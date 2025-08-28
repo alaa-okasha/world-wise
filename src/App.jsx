5e55f805e5db38b8a4804a0dc8fb50e1dd999e32
Alaa <aayyaser@hotmail.com>
2025-08-28 20:57:27 +0300

    building the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
+import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
 function App() {
   return (
     <BrowserRouter>
@@ -11,6 +12,8 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
