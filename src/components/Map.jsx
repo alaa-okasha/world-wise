d249392466fad6d21018c520c54a4d855ac62749
Alaa <aayyaser@hotmail.com>
2025-08-30 01:38:29 +0300

    building city component with dynamic route

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,23 @@
+import { useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  return (
+    <div className={styles.mapContainer}>
+      <h1>map</h1>
+      <p>
+        Position:{lat},{lng}
+      </p>
+      <button onClick={() => setSearchParams({ lat: 50, lng: 25 })}>
+        change position
+      </button>
+    </div>
+  );
 }
 
 export default Map;
