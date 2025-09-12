4799494b2505722498af962ed764c0d898c09fce
Alaa <aayyaser@hotmail.com>
2025-09-12 23:05:41 +0300

    fixing styles

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -14,11 +14,11 @@ function PageNav() {
         <li>
           <NavLink to="/pricing">Pricing</NavLink>
         </li>
-        <li>
+        {/* <li>
           <NavLink to="/login" className={styles.ctaLink}>
             Login
           </NavLink>
-        </li>
+        </li> */}
       </ul>
     </nav>
   );
